5889bff9d3220c4745af3085625778bd13db02f2
Frank Agnello <fja5533@rit.edu>
2020-10-17 15:19:28 -0400

    Ability to parse library book data

---
+++ src/Book.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    long isbn;
+    String title;
+    List<String> authorList;
+    String publisher;
+    String publishedDate;
+    int pages;
+    int copies;
+    int available;
+
+    public Book(long isbn, String title, String authorList, String publisher, String publishedDate,
+                int pages) {
+        this.isbn = isbn;
+        this.title = title;
+        this.publisher = publisher;
+        this.publishedDate = publishedDate;
+        this.pages = pages;
+        authorList.replace("{", "");
+        authorList.replace("}", "");
+        this.authorList = new ArrayList<>();
+        String[] tentativeList = authorList.split(",");
+        for(String author : tentativeList) {
+            this.authorList.add(author);
+        }
+    }
+}
