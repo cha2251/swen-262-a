47a1de776cc7747fd4e88559b42c4f59c480128e
chaub2251 <cha2251@g.rit.edu>
2020-10-19 14:39:59 -0400

    Added Visitor, BorrowedBooks, and fine functionaility

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,2 +1,54 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Visitor {
+    private int id;
+    private String firstName, lastName, address, phoneNumber;
+
+    private List<BorrowedBook> borrowedBooks;
+    private double finesOwed;
+
+    public Visitor(int id, String firstName, String lastName, String address, String phoneNumber){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+
+        borrowedBooks = new ArrayList<BorrowedBook>();
+    }
+
+    public void borrowBook(List<Book> books){
+        for(Book book : books){
+            borrowedBooks.add(new BorrowedBook(book, LocalDateTime.now()));
+        }
+    }
+
+    public List<BorrowedBook> findBorrowedBooks(){
+        return borrowedBooks;
+    }
+
+    public double returnBook(List<Book> books, LocalDateTime currentDate) {
+        double fine = 0;
+        for (Book book : books) {
+            double status = 0;
+            for (BorrowedBook borrowedBook : borrowedBooks) {
+                status = borrowedBook.returnBook(book, currentDate);
+                if (status >= 0) {
+                    borrowedBooks.remove(borrowedBooks);
+                    fine += status;
+                    break;
+                }
+            }
+        }
+        finesOwed += fine;
+        return fine;
+    }
+
+    public double payFine(double amount){
+        if (amount > finesOwed || amount < 0)return -1;
+        finesOwed -= amount;
+        return finesOwed;
+    }
 }
