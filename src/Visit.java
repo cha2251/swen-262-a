3e0320277e098cd260f3aa1dbab7453b0b36308e
chaub2251 <cha2251@g.rit.edu>
2020-10-19 14:58:55 -0400

    Added Visit class

--- src/Visit.java
+++ src/Visit.java
@@ -1,3 +1,29 @@
-public class Visit {
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
+import static java.time.format.DateTimeFormatter.*;
+
+public class Visit {
+    private Visitor visitor;
+    private LocalDateTime startDate;
+
+    public Visit(Visitor v, LocalDateTime date){
+        visitor = v;
+        startDate = date;
+    }
+
+    public String getStartDate(){
+        return startDate.format(ISO_LOCAL_DATE);
+    }
+
+    public String getStartTime(){
+        return startDate.format(ISO_LOCAL_TIME);
+    }
+
+    public String endVisit(LocalDateTime currentDate){
+        long hours = startDate.until( currentDate, ChronoUnit.HOURS );
+        long minutes = startDate.until( currentDate, ChronoUnit.MINUTES );
+        long seconds = startDate.until( currentDate, ChronoUnit.SECONDS );
+        return ""+hours+":"+minutes+":"+seconds;
+    }
 }
