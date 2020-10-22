44b87d73f036c8871f0b0fb84f005ca5818b3b80
Brandon Cunningham <btc2628@rit.edu>
2020-10-21 20:10:23 -0400

    Added documentation to Books

--- src/Book.java
+++ src/Book.java
@@ -6,6 +6,9 @@ public class Book {
     private String title, author, publisher;
     private Date publishDate;
 
+    /*
+    Used for the creation of a new book, includes all necessary information on the book
+     */
     public Book(int isbn, String title, String author, String publisher, Date publishDate, int pageCount, int numberOfCopies, int copiesAvailable){
         this.isbn = isbn;
         this.title = title;
@@ -16,15 +19,23 @@ public class Book {
         this.numberOfCopies = numberOfCopies;
         this.copiesAvailable = copiesAvailable;
     }
-
+    /*
+    When a Book is checked in, this increases the number of copies avaliable of that book by 1
+     */
     public void checkIn(){
         copiesAvailable++;
     }
 
+    /*
+    When a Book is checked out, this decreases the number of copies avilable of that book by 1
+    */
     public void checkOut(){
         copiesAvailable--;
     }
 
+    /*
+    Returns a String of the name of the author
+     */
     public String getAuthor(){
         return author;
     }
