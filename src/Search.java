e51b58f1140b1842d5e32fb4c802a133676340f3
Luna Battle <lcb7105>
2020-10-21 23:48:45 -0400

    added javadocs for Search interface

--- src/Search.java
+++ src/Search.java
@@ -1,5 +1,9 @@
 import java.util.List;
 
+/**
+ * An interface for a strategy that allows searching for books by
+ * specific criteria
+ */
 public interface Search {
     List<Book> result(List<Book> books);
 }
