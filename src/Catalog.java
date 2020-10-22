58768a844543b4782e999880ec960249a4a13a7f
Brandon Cunningham <btc2628@rit.edu>
2020-10-21 20:18:34 -0400

    Added documentation to Catalog and Ordering

--- src/Catalog.java
+++ src/Catalog.java
@@ -6,14 +6,23 @@ public class Catalog {
     private List<Book> bookList;
     private Ordering sortOrder;
 
+    /*
+    Creates a new arraylist of books.
+     */
     public Catalog(){
         bookList = new ArrayList<Book>();
     }
 
+    /*
+    Sets our sort order
+     */
     public void setSortOrder(Ordering newOrdering){
         sortOrder = newOrdering;
     }
 
+    /*
+    Used to sort the catalog in the sort order
+     */
     public List sortCatalog(Ordering ordering){
         return sortOrder.sort(bookList);
     }
--- src/Ordering.java
+++ src/Ordering.java
@@ -1,5 +1,9 @@
 import java.util.List;
 
 public interface Ordering {
+
+    /*
+    This will be the blueprint for sorting functions in our TitleOrdering class
+     */
     List sort(List<Book> bookList);
 }
