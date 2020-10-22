5728bfbe858c37a72d58aa9a0784221fb984eb24
Luna Battle <lcb7105>
2020-10-21 23:12:00 -0400

    added javadocs for SearchModifier class

--- src/SearchModifier.java
+++ src/SearchModifier.java
@@ -1,14 +1,26 @@
 import java.util.List;
 
+/**
+ * Runs searches on lists of books
+ */
 public class SearchModifier implements Search {
 
 
     protected Search search;
 
+    /**
+     * Constructor
+     * @param search
+     */
     public SearchModifier(Search search) {
         this.search = search;
     }
 
+    /**
+     * Runs the specified search on a given list of books
+     * @param books
+     * @return
+     */
     @Override
     public List<Book> result(List<Book> books) {
         return search.result(books);
