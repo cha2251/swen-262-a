ac9cb568862fe69c729da7c6d787527dbe9f03c0
Luna Battle <lcb7105>
2020-10-21 22:43:07 -0400

    added javadocs for SearchPublisher class

--- src/SearchPublisher.java
+++ src/SearchPublisher.java
@@ -1,15 +1,28 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implements a search strategy that finds a book by publisher
+ */
 public class SearchPublisher extends SearchModifier {
 
     private String publisher;
 
+    /**
+     * Constructor
+     * @param search
+     * @param publisher
+     */
     public SearchPublisher(Search search, String publisher) {
         super(search);
         this.publisher = publisher;
     }
 
+    /**
+     * returns a list of books sorted by publisher
+     * @param book
+     * @return
+     */
     public List<Book> result(List<Book> book) {
         List<Book> toReturn = new ArrayList<>();
         List<Book> toPrune = super.result(book);
