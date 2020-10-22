17c73dffaccc875a1efbeff2e60fc199b075bd01
Luna Battle <lcb7105>
2020-10-21 23:35:57 -0400

    added javadocs for SearchISBN class

--- src/SearchISBN.java
+++ src/SearchISBN.java
@@ -1,15 +1,28 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implements a search strategy that finds a book by ISBN
+ */
 public class SearchISBN extends SearchModifier {
 
     private String ISBN;
 
+    /**
+     * Constructor
+     * @param search
+     * @param ISBN
+     */
     public SearchISBN(Search search, String ISBN) {
         super(search);
         this.ISBN = ISBN;
     }
 
+    /**
+     * returns all books with the given ISBN
+     * @param book
+     * @return
+     */
     public List<Book> result(List<Book> book) {
         List<Book> toReturn = new ArrayList<>();
         List<Book> toPrune = super.result(book);
