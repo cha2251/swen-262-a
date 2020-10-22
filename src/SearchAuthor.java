6b0600130d18f811d448381fa759e973f473f3ed
Luna Battle <lcb7105>
2020-10-21 23:46:03 -0400

    added javadocs for SearchAuthor class

--- src/SearchAuthor.java
+++ src/SearchAuthor.java
@@ -1,14 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implements a search strategy that finds books by author
+ */
 public class SearchAuthor extends SearchModifier {
     private String authors;
 
+    /**
+     * Constructor
+     * @param search
+     * @param authors
+     */
     public SearchAuthor(Search search, String authors) {
         super(search);
         this.authors = authors;
     }
 
+    /**
+     * returns a list of books by a given author
+     * @param book
+     * @return
+     */
     public List<Book> result(List<Book> book) {
         List<Book> toReturn = new ArrayList<>();
         List<Book> toPrune = super.result(book);
