5c06a9b02d242cf6e4f06952b7d21b7fff16fa9b
Brandon Cunningham <btc2628@rit.edu>
2020-11-18 16:27:35 -0500

    Added undo/redo request classes

--- src/RedoRequest.java
+++ src/RedoRequest.java
@@ -1,5 +1,10 @@
 public class RedoRequest implements Request {
 
+    /**
+     * Request call for redoing a request
+     * @param args
+     * @return
+     */
     @java.lang.Override
     public String execute(String[] args) {
         return UndoRedo.getInstance().redoCommand();
--- src/UndoRequest.java
+++ src/UndoRequest.java
@@ -1,5 +1,10 @@
 public class UndoRequest implements Request {
 
+    /**
+     * Request call for undoing a request
+     * @param args
+     * @return
+     */
     @java.lang.Override
     public String execute(String[] args) {
         return UndoRedo.getInstance().undoCommand();
